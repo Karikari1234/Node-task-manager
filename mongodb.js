50e375fb85b1d56143758ee07df293d7ad616a09
Karikari1234 <42123922+Karikari1234@users.noreply.github.com>
2020-04-16 17:55:59 +0600

    Connected to MongoDB successfully!

---
+++ mongodb.js
@@ -0,0 +1,17 @@
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'MyDatabase'
+
+MongoClient.connect(connectionURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (error, client) => {
+    if (error) {
+        return console.log('Error !')
+    }
+
+    console.log('Connected Successfully!')
+})
