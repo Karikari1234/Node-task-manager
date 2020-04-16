62568ffa8ac3021d6972b97431ec069e9e3237c3
Karikari1234 <42123922+Karikari1234@users.noreply.github.com>
2020-04-16 18:38:19 +0600

    Created some sample collections

--- mongodb.js
+++ mongodb.js
@@ -1,9 +1,15 @@
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+const {
+    MongoClient,
+    ObjectID
+} = require('mongodb')
 
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'MyDatabase'
+
+const id = new ObjectID()
+console.log(id)
+console.log(id.getTimestamp())
 
 MongoClient.connect(connectionURL, {
     useNewUrlParser: true,
@@ -13,5 +19,50 @@ MongoClient.connect(connectionURL, {
         return console.log('Error !')
     }
 
-    console.log('Connected Successfully!')
+    const db = client.db(databaseName)
+
+    // db.collection('users').insertOne({
+    //     name: 'Naafiz',
+    //     age: 21
+    // }, (error, result) => {
+    //     if (error)
+    //         return console.log('Error in inserting')
+    //     console.log(result.ops)
+    // })
+
+    // db.collection('users').insertMany(
+    //     [{
+    //         name: 'Karim',
+    //         age: 25
+    //     }, {
+    //         name: 'Rarim',
+    //         age: 27
+    //     }], (error, result) => {
+    //         if (error) {
+    //             return console.log('error occured')
+    //         }
+
+    //         console.log(result.ops)
+    //     }
+    // )
+
+    // db.collection('tasks').insertMany(
+    //     [{
+    //         description: 'Clean the room',
+    //         completed: false
+    //     }, {
+    //         description: 'Wash the dishes',
+    //         completed: true
+    //     }, {
+    //         description: 'Fill water',
+    //         completed: true
+    //     }], (error, result) => {
+    //         if (error) {
+    //             return console.log('error occured')
+    //         }
+
+    //         console.log(result.ops)
+    //     }
+    // )
+
 })
