a77ac1f23688f96961112e9ff888aa4cafb9f98c
Karikari1234 <42123922+Karikari1234@users.noreply.github.com>
2020-04-18 12:47:37 +0600

    GET and POST Done

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ const port = process.env.PORT || 3000;
 app.post('/users', (req, res) => {
     const newUser = new User(req.body)
     newUser.save().then(() => {
+        res.status(201)
         res.send(newUser)
         console.log(newUser)
     }).catch((error) => {
@@ -25,9 +26,60 @@ app.post('/users', (req, res) => {
 
 app.post('/tasks', (req, res) => {
     const newTask = new Task(req.body)
-    newTask.save().then(() => res.send(newTask)).catch((error) => {
+    newTask.save().then(() => {
+        res.status(201)
+        res.send(newTask)
+    }).catch((error) => {
         res.status(400)
         res.send('Errrorrrr')
+    })
+})
+
+
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((e) => {
+        res.status(500)
+        res.send()
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404)
+        }
+
+        res.send(user)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((e) => {
+        res.status(500)
+        res.send()
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+
+    Task.findById(_id).then((task) => {
+        if (!task) {
+            return res.status(404)
+        }
+
+        res.send(task)
+    }).catch((e) => {
+        res.status(500).send()
     })
 })
 
